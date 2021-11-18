d507f94734de02efe00037146feb027a7612ee37
Timothy Nicdao <tmnicdao@hawaii.edu>
2021-11-17 16:57:32 -1000

    cleaning up repo

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py migrate
-web: gunicorn lonelystar.wsgi --log-file -
--- server_startup.sh
+++ server_startup.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-cd languageModel
-# python3 train_model.py
-cd ../backend
-python3 manage.py runserver 0.0.0.0:8000
-
-
+cd backend
+python manage.py runserver 0.0.0.0:8000
